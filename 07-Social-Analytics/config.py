85ad220318a298f7d8c550a458d16cbc8221dece
Jason Douangsanith <jasondouangsanith@dhcp-v094-080.mobile.uci.edu>
2018-06-14 17:53:42 -0700

    social media analysis hw

---
+++ 07-Social-Analytics/config.py
@@ -0,0 +1,4 @@
+# Twitter API Keysconsumer_key = "Evvpt1h8W6NqFGat2DCJyIQJE"
+consumer_secret = "tilsyGyNG371CbwOeYH8iGcJnL0v8xBZbCMCgrZtsCwiIv6SD4"
+access_token = "51217498-4WaAePQqz3vijoVdDZ9XtUoxmqPHGQNWVLxPchNdj"
+access_token_secret = "sHp0FDUeg2DSPJmJ37kZhNoKvnfOD2LiRJqv71etJgTeu"
